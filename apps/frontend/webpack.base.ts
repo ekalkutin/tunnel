8a6a35186c3fec3270a8e4f98448753299e79f6f
Evgenii Kalkutin <evgenii.kalkutin@gmail.com>
2025-10-06 21:20:08 +0300

    chore(): Init frontend repo

---
+++ apps/frontend/webpack.base.ts
@@ -0,0 +1,93 @@
+import path from 'node:path';
+
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { Configuration } from 'webpack';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const configuration: Configuration = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx',
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(webp)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+          {
+            loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          {
+            loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                namedExport: false,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      client: path.resolve(__dirname, 'src', 'client'),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
+
+export default configuration;
---
+++ apps/frontend/webpack.client.ts
@@ -0,0 +1,29 @@
+import path from 'node:path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
+import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import merge from 'webpack-merge';
+
+import sharedConfig from './webpack.base';
+
+const BUILD_PATH = path.resolve(__dirname, 'build', 'client');
+
+export default merge<Configuration & DevServerConfiguration>(sharedConfig, {
+  entry: './src/index.tsx',
+  target: 'web',
+  output: {
+    path: BUILD_PATH,
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+});
