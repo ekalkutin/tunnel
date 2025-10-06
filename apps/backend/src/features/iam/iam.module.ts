063985c8a4280ef0ed014f5cb914842732d956d4
Evgenii Kalkutin <evgenii.kalkutin@gmail.com>
2025-10-06 17:25:46 +0300

    test(): Test CI/CD after kubernetes restart

--- apps/backend/src/features/iam/iam.module.ts
+++ apps/backend/src/features/iam/iam.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AccountsController } from './api/accounts.controller';
+import { KeycloakService } from './application/keycloak/keycloak.service';
 
 @Module({
   controllers: [AccountsController],
+  providers: [KeycloakService],
 })
 export class IAMModule {}
